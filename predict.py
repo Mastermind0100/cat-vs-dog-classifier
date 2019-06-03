10cc18d0fcef52bf0f9aa315be143d9a8a47cf4a
Atharva Hudlikar <36445600+Mastermind0100@users.noreply.github.com>
2019-06-03 15:56:27 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ Created on Mon Jun  3 11:45:05 2019
 @author: AtharvaHudlikar
 """
 from keras.preprocessing import image
+
 model=load_model('modelwts.h5')
 
 test_image=image.load_img('/content/gdrive/My Drive/catdogclassifier/training/dogs/dog.104.jpg', target_size = (64, 64))
@@ -11,6 +12,7 @@ test_image=image.img_to_array(test_image)
 test_image=np.expand_dims(test_image, axis = 0)
 result=model.predict(test_image)
 training_set.class_indices
+
 if result[0][0] >= 0.5:
     prediction='Dog'
 else:
