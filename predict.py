e5e252cb4f3534db99c121b9c4de49b9381bf45d
Atharva Hudlikar <36445600+Mastermind0100@users.noreply.github.com>
2019-06-03 16:21:06 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ from keras.preprocessing import image
 
 model=load_model('modelwts.h5')
 
-test_image=image.load_img('/content/gdrive/My Drive/catdogclassifier/training/dogs/dog.104.jpg', target_size = (64, 64))
+test_image=image.load_img('directory/sample_image.jpg', target_size = (64, 64))
 test_image=image.img_to_array(test_image)
 test_image=np.expand_dims(test_image, axis = 0)
 result=model.predict(test_image)
