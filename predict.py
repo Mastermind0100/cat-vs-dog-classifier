e4c3a1e2f043fe262e3510abceb51dc5b60a0263
Atharva Hudlikar <36445600+Mastermind0100@users.noreply.github.com>
2019-06-03 11:51:08 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,18 @@
+"""
+Created on Mon Jun  3 11:45:05 2019
+
+@author: AtharvaHudlikar
+"""
+from keras.preprocessing import image
+model=load_model('modelwts.h5')
+
+test_image=image.load_img('/content/gdrive/My Drive/catdogclassifier/training/dogs/dog.104.jpg', target_size = (64, 64))
+test_image=image.img_to_array(test_image)
+test_image=np.expand_dims(test_image, axis = 0)
+result=model.predict(test_image)
+training_set.class_indices
+if result[0][0] >= 0.5:
+    prediction='Dog'
+else:
+    prediction='Cat'
+print(prediction)
